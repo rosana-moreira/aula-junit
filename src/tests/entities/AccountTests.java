3fdf4a6286a52e435fa26cdc0fa9f88f8db67b95
Rosana Moreira <rooh.moreiraa@gmail.com>
2023-01-23 19:23:00 -0300

    Criação da AccountFactory

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -4,13 +4,14 @@ package tests.entities;
 import entities.Account;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import tests.factory.AccountFactory;
 
 public class AccountTests {
     @Test
     public void depositShouldIncreaseBalanceWhenPositiveAmount() {
         double amount = 200.0;
         double expectedValue = 196.0;
-        Account acc = new Account(1L, 0.0);
+        Account acc = AccountFactory.createEmptyAccount();
         acc.deposit(amount);
         Assertions.assertEquals(expectedValue, acc.getBalance());
 
@@ -19,7 +20,7 @@ public class AccountTests {
     @Test
     public void depositShouldDoNothingWhenNegativeAmount() {
         double expectedValue = 100.00;
-        Account acc = new Account(1L, expectedValue);
+        Account acc = AccountFactory.createAccount(expectedValue);
         double amount = -200.0;
         acc.deposit(amount);
         Assertions.assertEquals(expectedValue, acc.getBalance());
